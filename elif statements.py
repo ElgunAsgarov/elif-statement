e1bb5c5991042bdf73c4b0fe21bbd534f811bb28
ElgunAsgarov <155174226+ElgunAsgarov@users.noreply.github.com>
2024-03-17 13:23:52 +0400

    Add files via upload

---
+++ elif statements.py
@@ -0,0 +1,15 @@
+#What is student's score
+score = int(56)
+#determining the grade.
+if score >= 90:
+    print ("Your grade is a A")
+elif score >= 80:
+    print ("Your grade is a B")
+elif score >= 70:
+    print ("Your grade is a C")
+elif score >= 60:
+    print ("Your grade is a D")
+elif score >= 50:
+    print ("Your grade is a F")
+else:
+    print ("You fail")
